a8dbd9b89d731c7299c2b8544176c310fdc895e3
Hy Nguyen <hynguyen2610@gmail.com>
2024-12-12 11:13:47 +0700

    Extract dev credentials to .env This commit contains once the .env file

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,14 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // src/lib/db.ts
 import { Pool } from 'pg';
+import 'dotenv/config';
 
 const pool = new Pool({
-  user: 'myuser',
-  host: 'localhost',
-  database: 'metrics',
-  password: 'mypassword',
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT!, 10),
 });
 
 export const query = async (text: string, params?: any[]) => {
